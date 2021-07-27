a7069b81f17910a4f0bdb999dfaa5fd0c727b2e0
bina3c <48756392+Bina-man@users.noreply.github.com>
2021-07-28 00:57:58 +0300

    Simple update

--- scripts/plots.py
+++ scripts/plots.py
@@ -6,7 +6,7 @@ import pandas as pd
 def plot_hist(df: pd.DataFrame, column: str, color: str) -> None:
     plt.figure(figsize=(9, 7))
     sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
-    plt.title(f'Distribution of {column}', size=20, fontweight='bold')
+    plt.title(f'Distribution of {column}', size=50, fontweight='bold')
     plt.show()
 
 
