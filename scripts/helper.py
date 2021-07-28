9693bbb9ddcb0bdac93d230b013d5a5d2ca78784
bina3c <48756392+Bina-man@users.noreply.github.com>
2021-07-28 22:06:01 +0300

    Scripts

--- scripts/helper.py
+++ scripts/helper.py
@@ -1,67 +1,85 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import Normalizer, MinMaxScaler
+from sklearn.preprocessing import Normalizer, MinMaxScaler, StandardScaler
+import pickle
+import dvc.api
+from app_logger import App_Logger
 
 
-class TelecomHelper:
-  
-  def __init__(self):
-    pass
-  
-  def read_csv(self, csv_path, missing_values=[]):
-    try:
-        df = pd.read_csv(csv_path, na_values=missing_values)
-        print("file read as csv")
+app_logger = App_Logger("helper.log").get_app_logger()
+
+
+class Helper:
+
+    def __init__(self):
+        self.logger = App_Logger("helper.log").get_app_logger()
+
+    def read_model(self, file_name):
+        with open(f"../models/{file_name}.pkl", "rb") as f:
+            self.logger.info(f"Model loaded from {file_name}.pkl")
+            return pickle.load(f)
+
+    def write_model(self, file_name, model):
+        with open(f"../models/{file_name}.pkl", "wb") as f:
+            self.logger.info(f"Model dumped to {file_name}.pkl")
+            pickle.dump(model, f)
+
+    def read_csv(self, csv_path, missing_values=[]):
+        try:
+            df = pd.read_csv(csv_path, na_values=missing_values)
+            print("file read as csv")
+            self.logger.info(f"file read as csv from {csv_path}")
+            return df
+        except FileNotFoundError:
+            print("file not found")
+            self.logger.error(f"file not found, path:{csv_path}")
+
+    def save_csv(self, df, csv_path):
+        try:
+            df.to_csv(csv_path, index=False)
+            print('File Successfully Saved.!!!')
+            self.logger.info(f"File Successfully Saved to {csv_path}")
+
+        except Exception:
+            print("Save failed...")
+            self.logger.error(f"saving failed")
+
         return df
-    except FileNotFoundError:
-        print("file not found")
-  
-  def save_csv(self, df, csv_path):
-    try:
-        df.to_csv(csv_path, index=False)
-        print('File Successfully Saved.!!!')
 
-    except Exception:
-        print("Save failed...")
+    def get_data(tag, path='data/data.csv', repo='https://github.com/daniEL2371/pharmaceutical-sales-prediction'):
+        rev = tag
+        data_url = dvc.api.get_url(path=path, repo=repo, rev=rev)
+        df = pd.read_csv(data_url)
+        app_logger.info(f"Read data from {path}, version {tag}")
 
-    return df
-    
-  def percent_missing(self, df: pd.DataFrame) -> float:
+        return df
 
-    totalCells = np.product(df.shape)
-    missingCount = df.isnull().sum()
-    totalMissing = missingCount.sum()
-    return round((totalMissing / totalCells) * 100, 2)
-  
-  def percent_missing_for_col(self, df: pd.DataFrame, col_name: str) -> float:
-    total_count = len(df[col_name])
-    if total_count <= 0:
-        return 0.0
-    missing_count = df[col_name].isnull().sum()
+    def percent_missing(self, df: pd.DataFrame) -> float:
 
-    return round((missing_count / total_count) * 100, 2)
-  
-  def convert_bytes_to_megabytes(self, df: pd.DataFrame, bytes_data):
+        totalCells = np.product(df.shape)
+        missingCount = df.isnull().sum()
+        totalMissing = missingCount.sum()
+        return round((totalMissing / totalCells) * 100, 2)
 
-    megabyte = 1*10e+5
-    megabyte_col = df[bytes_data] / megabyte
+    def percent_missing_for_col(self, df: pd.DataFrame, col_name: str) -> float:
+        total_count = len(df[col_name])
+        if total_count <= 0:
+            return 0.0
+        missing_count = df[col_name].isnull().sum()
 
-    return megabyte_col
-  
-  def normalizer(self, df, columns):
-    norm = Normalizer()
-    return pd.DataFrame(norm.fit_transform(df), columns=columns)
+        return round((missing_count / total_count) * 100, 2)
 
+    def normalizer(self, df, columns):
+        norm = Normalizer()
+        return pd.DataFrame(norm.fit_transform(df), columns=columns)
 
-  def scaler(self, df, columns):
-    minmax_scaler = MinMaxScaler()
-    return pd.DataFrame(minmax_scaler.fit_transform(df), columns=columns)
+    def scaler(self, df, columns, mode="minmax"):
+        if (mode == "minmax"):
+            minmax_scaler = MinMaxScaler()
+            return pd.DataFrame(minmax_scaler.fit_transform(df), columns=columns)
+        elif (mode == "standard"):
+            scaler = StandardScaler()
+            return pd.DataFrame(scaler.fit_transform(df), columns=columns)
 
-
-  def scale_and_normalize(self, df, columns):
-    return self.normalizer(self.scaler(df, columns), columns)
-  
-  
-
-  
-  
+    def scale_and_normalize(self, df, columns, sclaer_mode="minmax"):
+        return self.normalizer(self.scaler(df, columns, sclaer_mode), columns)
